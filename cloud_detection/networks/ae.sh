a5fd56ac8d43a415689a372bec9e09e325c5b223
Jerry Schonenberg <j.r.q.schonenberg@gmail.com>
2022-12-19 16:01:45 +0100

    Updated relative paths

--- cloud_detection/networks/ae.sh
+++ cloud_detection/networks/ae.sh
@@ -1,4 +1,4 @@
 # Running 3 replications on the 38-Cloud dataset
-python3 main.py --summary --dataset_train "../datasets/38-cloud/train/" --dataset_test "../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 8
-python3 main.py --summary --dataset_train "../datasets/38-cloud/train/" --dataset_test "../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 42
-python3 main.py --summary --dataset_train "../datasets/38-cloud/train/" --dataset_test "../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 1024
+python3 main.py --summary --dataset_train "../../datasets/38-cloud/train/" --dataset_test "../../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 8
+python3 main.py --summary --dataset_train "../../datasets/38-cloud/train/" --dataset_test "../../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 42
+python3 main.py --summary --dataset_train "../../datasets/38-cloud/train/" --dataset_test "../../datasets/38-cloud/test/" --epochs 64 --batch_size 64 --workers 4 --augmentation --mp --model AE --seed 1024
